39915da9e9226079785a5d6bfef9714f68a9f571
vmad1820 <48395615+vmad18@users.noreply.github.com>
2021-03-19 23:03:27 -0400

    Add files via upload

---
+++ NLP for Detecting Spam/SpamDetector.py
@@ -0,0 +1,12 @@
+import numpy
+import pickle
+
+def spamDetect(string):
+    #TODO create parser for the email
+    
+    #Load in model
+    filename = "spam_detection_classifier"
+    spamclassifier = pickle.load(open(filename,'rb'))
+
+    #Make prediction 
+    return spamclassifier.predict(string)
